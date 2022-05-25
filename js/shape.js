acc9bee2f1a57ac6712a3e78030bea44d36d669a
sp4837 <105900684+sp4837@users.noreply.github.com>
2022-05-25 01:26:50 -0400

    added some shapes, still cannot check code on browser
    added shapes, will need code checked, and sprites importation still needs to be learned; also fix differences

--- js/shape.js
+++ js/shape.js
@@ -4,6 +4,12 @@ class Square {
     var x, y = ;
     var perimeter [(x, y)]
 }
+// Think we need this part somewhere, but I'm not sure,
+// var canvas = document.querySelector('canvas')
+// var c = canvas.getContext('2d')
+// to import sprites
+    // import ---- from
+
 
 class Point {
     constructor(x, y) {
@@ -38,6 +44,59 @@ class Circle {
         canvas.fill()
     }
 }
-
 const Circle = new Circle (100, 100, 5, 'red')
 player.draw()
+
+
+class Rectangle {
+    constructor() {
+        this.position = {
+            x: 55
+            y: 55
+        }
+        this.width = 20
+        this.height = 40
+    }
+
+    draw(canvas) {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        ctx.fillStyle = 'green'
+    }
+}
+const Rectangle = new Rectangle()
+Rectangle.draw()
+// For above, is it Rectangle.draw or player.draw (if latter, change all below classes)
+
+class Sqaure {
+    constructor() {
+        this.position = {
+            x: 115
+            y: 115
+        }
+        this.length = 16
+    }
+
+    draw(canvas) {
+        ctx.fillRect(this.position.x, this.position.y, this.length, this.length)
+        ctx.fillStyle = 'orange'
+    }
+}
+const Square = new Square()
+Square.draw()
+
+
+class ComplexPolygon {
+    var SideCount = 8
+    scalar = 10
+    Xcenter = 10
+    Ycenter = 10
+
+    ctx.beginpath()
+    ctx.moveTo (Xcenter + Math.cos(0) * scalar, Ycenter + Math.sin(0) * scalar)
+    for (var i = 1; i <= SideCount; i += 1) {
+        ctx.lineTo (Xcenter + Math.cos(2 * Math.pi * i / SideCOunt) * scalar, Ycenter + Math.sin(2 * Math.Pi * i /SideCount) * scalar)
+        ctx.fillStyle = 'purple'
+    }
+}
+
+ComplexPolygon.draw()
